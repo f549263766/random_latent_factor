fix: change the calculation of the formula, get the best performance

module/model/pyt_model.py
@@ -18,41 +18,35 @@ class PtyModel:
         rnd = np.random.RandomState()
 
         # Initialize the w → [input_dim, hidden_dim]
-        self.w = rnd.uniform(-1, 1, (self.num_input_dim, self.num_hidden_dim))
+        self.w = rnd.uniform(-2, 2, (self.num_input_dim, self.num_hidden_dim))
         # Initialize the bias → [hidden_dim]
-        self.bias = np.array([rnd.uniform(-0.4, 0.4) for _ in range(num_hidden_dim)], dtype=float)
+        self.bias = np.array([rnd.uniform(-0.2, 0.2) for _ in range(num_hidden_dim)], dtype=float)
         # Initialize the beta → [hidden_dim, output_dim]
         self.beta = np.zeros([self.num_hidden_dim, self.num_output_dim], dtype=float)
         # Initialize the gamma → [hidden_dim, output_dim]
         self.gamma = np.zeros([self.num_hidden_dim, self.num_output_dim], dtype=float)
+        # Initialize the mu → [hidden_dim, output_dim]
+        self.mu = np.zeros([self.num_hidden_dim, self.num_output_dim], dtype=float)
         # Initialize the hidden output
         self.h = None
 
-    def fix(self, x, y, x_co, y_co):
+    def fix(self, x, y, x_co, y_co, x_mistrust, y_mistrust):
         # beta-step
         h_matrix = np.asarray(self.activation(np.dot(x, self.w) + self.bias))
-        h_matrix_inverse = np.linalg.inv(2e-5 + np.dot(h_matrix.T, h_matrix))
-        h_matrix_output = np.dot(h_matrix.T, y)
-        self.beta = np.dot(h_matrix_inverse, h_matrix_output)
+        h_matrix_inverse = np.dot(np.linalg.inv(np.dot(h_matrix.T, h_matrix) + np.asarray(1e-5)), h_matrix.T)
+        self.beta = np.dot(h_matrix_inverse, y)
         # gamma-step
-        gamma_matrix = np.asarray(self.activation(np.dot(x, self.w) + self.bias))
-        gamma_matrix_inverse = np.linalg.inv(2e-5 + np.dot(gamma_matrix.T, gamma_matrix))
-        gamma_matrix_output = np.dot(gamma_matrix.T, y_co)
-        self.gamma = np.dot(gamma_matrix_inverse, gamma_matrix_output)
+        self.gamma = np.dot(h_matrix_inverse, y_co)
+        # mu-step
+        self.mu = np.dot(h_matrix_inverse, y_mistrust)
         # h-step
-        h_part1 = 2e-5 * (np.dot(y, self.beta.T) + np.dot(y_co, self.gamma.T))
-        h_part2 = np.asarray(2e-5) + np.dot(self.beta, self.beta.T)
-        h_part3 = np.asarray(2e-5) + np.dot(self.gamma, self.gamma.T)
-        try:
-            h_part4 = np.linalg.inv(h_part2 + h_part3)
-        except np.linalg.LinAlgError:
-            h_part4 = np.linalg.pinv(h_part2 + h_part3)
-        h_part5 = np.dot(h_part1, h_part4)
-        self.h = h_part5
+        self.h = np.dot((np.dot(x, self.beta.T) + np.dot(x_co, self.gamma.T) + np.dot(x_mistrust, self.mu.T)),
+                        np.linalg.inv(np.dot(self.beta, self.beta.T) + np.dot(self.gamma, self.gamma.T) +
+                                      np.dot(self.mu, self.mu.T) + np.asarray(1e-5)))
 
         return self.h, self.beta, self.gamma
 
     def __call__(self, x):
-        output = np.dot(self.h, 0.5 * self.beta + 0.5 * self.gamma)
+        output = np.dot(self.h, self.beta)
 
         return output

module/model/rlfn.py
@@ -29,18 +29,9 @@ class Rlfn:
     def fix(self, x, y):
         # beta-step
         h_matrix = np.asarray(self.activation(np.dot(x, self.w) + self.bias))
-        h_matrix_inverse = np.linalg.inv(1e-5 + np.dot(h_matrix.T, h_matrix))
-        h_matrix_output = np.dot(h_matrix.T, y)
-        self.beta = np.dot(h_matrix_inverse, h_matrix_output)
+        self.beta = np.dot(np.dot(np.linalg.inv(np.dot(h_matrix.T, h_matrix) + np.asarray(1e-5)), h_matrix.T), y)
         # h-step
-        h_part1 = np.dot(y, self.beta.T)
-        h_part2 = np.asarray(1e-5)
-        h_part3 = h_part2 + np.dot(self.beta, self.beta.T)
-        try:
-            h_part4 = np.linalg.inv(h_part3)
-        except np.linalg.LinAlgError:
-            h_part4 = np.linalg.pinv(h_part3)
-        self.h = np.dot(h_part1, h_part4)
+        self.h = np.dot(np.dot(x, self.beta.T), np.linalg.inv(np.dot(self.beta, self.beta.T) + np.asarray(1e-5)))
 
         return self.h, self.beta
 

utlis/train.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import numpy as np
 import torch
 import torch.nn as nn
 import matplotlib.pyplot as plt
@@ -9,6 +10,7 @@ from module.model.rlfn import Rlfn
 from module.model.pyt_model import PtyModel
 from module.datasets.test_regression import RegressionDataset
 from module.datasets.pty3_ratings import Pty3Rating
+from utlis.logger import get_root_logger
 
 
 def command_line():
@@ -20,7 +22,7 @@ def command_line():
     parser.add_argument("-lb", "--hidden_dim_lb", type=int, default=5,
                         help="The minimum number of neurons in the hidden layer of SLFN."
                              "Default to 5.")
-    parser.add_argument("-ub", "--hidden_dim_ub", type=int, default=50,
+    parser.add_argument("-ub", "--hidden_dim_ub", type=int, default=100,
                         help="The maximum number of neurons in the hidden layer of SLFN."
                              "Default to 100.")
     parser.add_argument("-interval", "--incremental_interval", type=int, default=5,
@@ -38,26 +40,32 @@ def command_line():
 
 def train(cfg):
     # data_set = RegressionDataset()
-    data_set = Pty3Rating()
-    x_train, y_train, x_trust_train, y_trust_train = data_set.x, data_set.y, data_set.x_trust, data_set.y_trust
+    data_set = Pty3Rating(user_rating_data_path="D:/QYZ/Code/random_latent_factor/dataset/D3-ratings.xlsx",
+                          user_trust_data_path="D:/QYZ/Code/random_latent_factor/dataset/data3trust.xlsx",
+                          logger=logger)
+    x, y, x_trust, y_trust, x_mistrust, y_mistrust = data_set.x, data_set.y, data_set.x_trust, data_set.y_trust, \
+                                                     data_set.x_mistrust, data_set.y_mistrust
 
     # plt.plot(x_train, y_train, 'or')
 
     for num_hidden_dim in range(cfg.hidden_dim_lb, cfg.hidden_dim_ub, cfg.incremental_interval):
-        # slfn = Elm(num_input_dim=x_train.shape[1],
-        #            num_hidden_dim=num_hidden_dim,
-        #            num_output_dim=y_train.shape[1])
-        # slfn = Rlfn(num_input_dim=x_train.shape[1],
-        #             num_hidden_dim=num_hidden_dim,
-        #             num_output_dim=y_train.shape[1])
-        slfn = PtyModel(num_input_dim=x_train.shape[1],
-                        num_hidden_dim=num_hidden_dim,
-                        num_output_dim=y_train.shape[1])
+        try:
+            # slfn = Elm(num_input_dim=x.shape[1],
+            #            num_hidden_dim=num_hidden_dim,
+            #            num_output_dim=y.shape[1])
+            # slfn = Rlfn(num_input_dim=x.shape[1],
+            #             num_hidden_dim=num_hidden_dim,
+            #             num_output_dim=y.shape[1])
+            slfn = PtyModel(num_input_dim=x.shape[1],
+                            num_hidden_dim=num_hidden_dim,
+                            num_output_dim=y.shape[1])
 
-        # _ = slfn.fix(x_train, y_train)
-        _ = slfn.fix(x_train, y_train, x_trust_train, y_trust_train)
-        predict = slfn(x_train)
-        rmse = metrics.mean_squared_error(y_train, predict) ** 0.5
+            # _ = slfn.fix(x, y)
+            _ = slfn.fix(x, y, x_trust, y_trust, x_mistrust, y_mistrust)
+        except np.linalg.LinAlgError:
+            continue
+        predict = slfn(x)
+        rmse = metrics.mean_squared_error(y, predict) ** 0.5
         print(f"number of layer {num_hidden_dim}, rmse: {rmse}")
 
     #     plt.plot(x_train, predict)
@@ -69,4 +77,5 @@ def train(cfg):
 
 if __name__ == '__main__':
     args = command_line()
+    logger = get_root_logger()
     train(args)